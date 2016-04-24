8057f09ab0bd5fe1cc49c298c38e0855b31e4b09
Miheer Dewaskar <miheerdew@gmail.com>
2016-04-24 20:24:54 +0530

    Some progress

--- presentation/mean.lua
+++ presentation/mean.lua
@@ -8,50 +8,28 @@ require("mylib")
     --end
 --end
 
-local count = 0
-
-local function printNextLine(token, nodes, weights, lim)
-    local last, math=false
-    if lim then
-        lim = "\\to "..lim
-        last=true
-    else
-        lim = ""
-    end
-
-    local math_fmt="\\[\\frac{%s}{%d} %s \\]"
-    if #weights == 0 then
-        math=""
-    else
-        math=string.format(math_fmt, table.concat(weights, ""), #weights, lim)
-    end
-
-    if last then -- This is last
-        tsf("\\only<.(%d)>{\\settok{%s}}\\only<.(%d)->{%s \\enskip $\\ldots$ \\\\ %s }",
-                count, token, count + 1,
-                table.concat(nodes," \\enskip "),
-                math)
-        tsf("\\addtocounter{beamerpauses}{%d}", count+1)
-    else
-        tsf("\\only<.(%d)>{\\settok{%s}}\\only<.(%d)>{%s \\\\ %s }",
-                    count, token, count + 1,
-                    table.concat(nodes," \\enskip "),
-                    math)
-        count = count + 1
-    end
-end
-
 function animateMeanPath(items, lim)
-    count=0
+    local x
     local nodes, weights, l={}, {}
+    local s1, num, den={}, {}, {}
     for i,item in ipairs(items) do
         table.insert(nodes, item.node)
-        if i == #items then
-            l = lim
+        x=interp([[\only<.($i)>{\SetMyToken{$token}}\onslide<.($i)->{$arrow$node}]],
+                            {i=i, token=item.token, node=item.node,
+                             arrow=i>1 and string.format("$\\xrightarrow{%s}$", weights[i-1]) or "",
+                             })
+        table.insert(s1,x)
+        if i > 1 then
+            x=interp([[\onslide<.($i)->{$weight}]], {i=i, weight=weights[i-1]})
+            table.insert(num,x)
+            x=interp([[\only<.($i)$range>{$count}]], {i=i, count=i-1, range= i==#items and "-" or ""})
+            table.insert(den,x)
         end
-        printNextLine(item.token, nodes, weights, l)
         table.insert(weights, item.weight)
     end
+    tsi([[$path \onslide<.($count)->{$\ldots$} \begin{align*}\onslide<.(2)->{\frac{$num}{$den} \onslide<.($count)->{$lim}} \end{align*}]],
+            {path=table.concat(s1,""), num=table.concat(num,""), den=table.concat(den,""), count=#items+1, lim=lim})
+    tsi([[\addtocounter{beamerpauses}{$count}]],{count=#items+1})
 end
 
 function CmdAnimateMeanPath(input, lim)
